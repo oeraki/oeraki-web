4f92d981fc379a5bfeee6d9bde0a5bae218ee8c9
Nhat-Quang Tran (N-Q) <trannhatquang3112@gmail.com>
2019-04-03 14:12:21 -0700

    add login form layout

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+
+import './assets/vendor/nucleo/css/nucleo.css';
+import './assets/vendor/@fortawesome/fontawesome-free/css/all.min.css';
+import './assets/scss/argon-dashboard-react.scss';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
