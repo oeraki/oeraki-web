10cdd96e6c6c10b0468580e910c73fcc2444233c
Nhat-Quang Tran (N-Q) <trannhatquang3112@gmail.com>
2019-05-12 15:35:27 -0700

    hardcode for evets

--- src/dashboardRoutes.js
+++ src/dashboardRoutes.js
@@ -2,17 +2,19 @@
 // import Profile from "views/examples/Profile.jsx";
 // import Maps from "views/examples/Maps.jsx";
 // import Register from "views/examples/Register.jsx";
-import Feed from './components/Feed';
+
+// import Feed from './components/Feed';
 import Profile from './components/Profile';
+import Events from './components/Events';
 
 var routes = [
-    {
-        path: "/feed",
-        name: "Feed",
-        icon: "ni ni-tv-2 text-primary",
-        component: Feed,
-        layout: "/dashboard"
-    },
+    // {
+    //     path: "/feed",
+    //     name: "Feed",
+    //     icon: "ni ni-tv-2 text-primary",
+    //     component: Feed,
+    //     layout: "/dashboard"
+    // },
     {
         path: "/profile",
         name: "Profile",
@@ -20,5 +22,13 @@ var routes = [
         component: Profile,
         layout: "/dashboard"
     },
+    {
+        path: "/events",
+        name: "Events",
+        icon: "ni ni-notification-70 text-primary",
+        component: Events,
+        layout: "/dashboard"
+    },
+
 ];
 export default routes;
